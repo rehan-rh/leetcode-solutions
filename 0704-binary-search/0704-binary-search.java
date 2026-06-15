4225c0690b9e68464e56dc628553f8b1a3512dda
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-06-15 15:46:22 +0530

    Time: 0 ms (100%), Space: 47.8 MB (97.45%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int h = nums.length-1;
+        while(l<=h)
+        {
+            int mid = l+(h-l)/2;
+            if(nums[mid]==target) return mid;
+            if(target<nums[mid])
+            {
+                h = mid-1;
+            }
+            else
+            {
+                l = mid+1;
+            }
+        }
+        return -1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

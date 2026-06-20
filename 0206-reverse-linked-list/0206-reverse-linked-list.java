0e4233843eb8ccc8627940bac2aa6c394fddd0a8
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-06-20 19:11:38 +0530

    Time: 0 ms (100%), Space: 44.4 MB (13.7%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        while(curr!=null)
+        {
+            ListNode nn = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nn;
+        }
+        return prev;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

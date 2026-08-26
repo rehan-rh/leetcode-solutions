d43d4a80e62c5822bf34ce37ecd26174bee037fb
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-08-26 16:53:35 +0530

    Time: 30 ms (87.85%), Space: 59.1 MB (74.49%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,12 +1,11 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int n = nums.length;
         List<List<Integer>> ml = new ArrayList<>();
+        int n = nums.length;
         Arrays.sort(nums);
         for(int i=0 ; i<n ; i++)
         {
             if(i>0 && nums[i-1]==nums[i]) continue;
-
             int j = i+1;
             int k = n-1;
             while(j<k)
@@ -23,8 +22,8 @@ class Solution {
                 else
                 {
                     ml.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
                     k--;
+                    j++;
                     while(j<k && nums[j-1]==nums[j]) j++;
                     while(j<k && nums[k+1]==nums[k]) k--;
                 }
@@ -33,7 +32,3 @@ class Solution {
         return ml;
     }
 }
-
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

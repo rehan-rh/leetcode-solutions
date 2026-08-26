766e7db722e176a5ce1ab5f3ea042622be59dc60
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-08-26 14:46:38 +0530

    Time: 1 ms (99.95%), Space: 77.4 MB (19.76%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,16 +1,12 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int cs = 0;
         int ms = Integer.MIN_VALUE;
+        int cs = 0;
         for(int e : nums)
         {
-            cs = Math.max(e, cs+e);
+            cs = Math.max(cs+e, e);
             ms = Math.max(ms, cs);
         }
         return ms;
     }
 }
-
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

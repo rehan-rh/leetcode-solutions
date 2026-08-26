b6edf798ed5f4cf2a4694c6a8abf593df6e41512
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-08-26 10:30:11 +0530

    Time: 2 ms (91.93%), Space: 47.8 MB (38.44%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,20 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i = 0;
-        for(int j=0 ; j<nums.length ; j++)
+        int n = nums.length;
+        int j = 0;
+        for(int i=0 ; i<n ; i++)
         {
-            if(nums[j]!=0)
+            if(nums[i]!=0)
             {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                i++;
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j++;
             }
         }
-
     }
 }
-
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

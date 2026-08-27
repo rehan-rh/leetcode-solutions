704d11ef58f0c329f6ea95b9988cd496d44c7305
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-08-27 12:20:38 +0530

    Time: 1 ms (60.79%), Space: 43.1 MB (83.02%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -5,18 +5,18 @@ class Solution {
         if(n==1) return strs[0];
         String s1 = strs[0];
         String s2 = strs[n-1];
-        if(s1.equals(s2)) return s1;
-        int i = 0;
+        int i=0;
         StringBuilder sb = new StringBuilder();
-        while(i<s1.length() && i<s2.length() && s1.charAt(i)==s2.charAt(i))
+        while(i<s1.length() && i<s2.length())
         {
-            sb.append(s1.charAt(i));
+            if(s1.charAt(i)==s2.charAt(i))
+            {
+                sb.append(s1.charAt(i));
+            }
+            else
+            break;
             i++;
         }
         return sb.toString();
     }
 }
-
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

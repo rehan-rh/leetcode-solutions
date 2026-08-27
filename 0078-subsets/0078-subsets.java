290f0fe9d5e211751b6dd8b7285f431a7d706514
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-08-27 20:22:25 +0530

    Time: 1 ms (86.51%), Space: 44.3 MB (20.41%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,15 +1,15 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ml = new ArrayList<>();
         int n = nums.length;
-        for(int num=0 ; num<(1<<n) ; num++)
+        List<List<Integer>> ml = new ArrayList<>();
+        for(int i=0 ; i<(1<<n) ; i++)
         {
             List<Integer> al = new ArrayList<>();
-            for(int i=0 ; i<n ; i++)
+            for(int j=0 ; j<n ; j++)
             {
-                if((num&(1<<i))!=0)
+                if((i&(1<<j))!=0)
                 {
-                    al.add(nums[i]);
+                    al.add(nums[j]);
                 }
             }
             ml.add(al);
@@ -17,7 +17,3 @@ class Solution {
         return ml;
     }
 }
-
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

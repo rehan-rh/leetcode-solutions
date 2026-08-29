b66535d52b2840bdd50634ed75ef95f7ca6eab46
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-08-29 19:29:50 +0530

    Time: 5 ms (8.33%), Space: 49.2 MB (17.41%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,18 +1,18 @@
 class Solution {
-    void bfs(int s, List<List<Integer>> adj, int[] vis)
+    void bfs(List<List<Integer>> adj, int vis[], int s)
     {
+        vis[s] = 1;
         Queue<Integer> q = new ArrayDeque<>();
         q.offer(s);
-        vis[s] = 1;
         while(!q.isEmpty())
         {
-            int u = q.poll();
-            for(int v : adj.get(u))
+            int v = q.poll();
+            for(int u : adj.get(v))
             {
-                if(vis[v]!=1)
+                if(vis[u]!=1)
                 {
-                    vis[v] = 1;
-                    q.offer(v);
+                    vis[u] = 1;
+                    q.offer(u);
                 }
             }
         }
@@ -20,33 +20,31 @@ class Solution {
     public int findCircleNum(int[][] isConnected) {
         List<List<Integer>> adj = new ArrayList<>();
         int n = isConnected.length;
-        int m = isConnected[0].length;
-        for(int i=0 ; i<n ; i++) adj.add(new ArrayList<>());
         for(int i=0 ; i<n ; i++)
         {
-            for(int j=0 ; j<m ; j++)
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0 ; i<n ; i++)
+        {
+            for(int j=0 ; j<n ; j++)
             {
                 if(isConnected[i][j]==1)
                 {
                     adj.get(i).add(j);
+                    adj.get(j).add(i);
                 }
             }
         }
-
         int vis[] = new int[n];
         int c = 0;
         for(int i=0 ; i<n ; i++)
         {
             if(vis[i]!=1)
             {
-                bfs(i, adj, vis);
                 c++;
+                bfs(adj, vis, i);
             }
         }
         return c;
     }
 }
-
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna

d4bd97e3a11f68a04376085c6fffcbab5a916eb2
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-09-02 12:04:10 +0530

    Time: 5 ms (80.38%), Space: 44.7 MB (53.52%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -21,8 +21,27 @@ class Solution {
         if((ts-dif)<0 || (ts-dif)%2==1) return 0;
         int n = nums.length;
         int dp[][] = new int[n][t+1];
-        for(int a[]:dp) Arrays.fill(a, -1);
-        return f(n-1, t, dp, nums);
+        if(nums[0]==0)
+        dp[0][0] = 2;
+        else
+        {
+            dp[0][0] = 1;
+            if(nums[0]<=t)
+            dp[0][nums[0]] = 1;
+        }
+        for(int i=1 ; i<n ; i++)
+        {
+            for(int j=0 ; j<t+1 ; j++)
+            {
+                int p = 0;
+                if(nums[i]<=j)
+                p = dp[i-1][j-nums[i]];
+                int np = dp[i-1][j];
+                dp[i][j] = p+np;
+            }
+        }
+        return dp[n-1][t];
+
     }
     public int findTargetSumWays(int[] nums, int target) {
         return df(nums, target);

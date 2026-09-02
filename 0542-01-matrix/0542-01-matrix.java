887807e931f32bb87af6e8f0089a5cf3adccf325
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-09-02 17:40:46 +0530

    Time: 14 ms (71.06%), Space: 50 MB (5.27%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,4 +1,4 @@
-class pair
+class pair 
 {
     int r;
     int c;
@@ -23,16 +23,14 @@ class Solution {
             {
                 if(mat[i][j]==0)
                 {
-                    q.offer(new pair(i, j, 0));
-                    dis[i][j] = 0;
                     vis[i][j] = 1;
+                    dis[i][j] = 0;
+                    q.offer(new pair(i, j, 0));
                 }
             }
         }
-
-        int row[] = {-1, 0, 1, 0};
-        int col[] = {0, 1, 0, -1};
-
+        int row[] = {0, -1, 0, 1};
+        int col[] = {-1, 0, 1, 0};
         while(!q.isEmpty())
         {
             pair p = q.poll();
@@ -41,9 +39,9 @@ class Solution {
             int d = p.d;
             for(int i=0 ; i<4 ; i++)
             {
-                int nr = r+row[i];
-                int nc = c+col[i];
-                if(nr>=0 && nr<n && nc>=0 && nc<m && vis[nr][nc]==0 && mat[nr][nc]==1)
+                int nr = row[i]+r;
+                int nc = col[i]+c;
+                if(nr>=0 && nr<n && nc>=0 && nc<m && vis[nr][nc]==0)
                 {
                     vis[nr][nc] = 1;
                     dis[nr][nc] = d+1;
@@ -54,7 +52,3 @@ class Solution {
         return dis;
     }
 }
-
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna

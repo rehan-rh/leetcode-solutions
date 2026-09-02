6cc0d44581bffdd376a572fca999f57c0838cf9f
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-09-02 21:17:05 +0530

    Time: 26 ms (37.34%), Space: 60.3 MB (88.29%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,31 +1,27 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        int n = graph.length;
-        int indegree[] = new int[n];
         List<List<Integer>> adj = new ArrayList<>();
+        int n = graph.length;
         for(int i=0 ; i<n ; i++)
         {
             adj.add(new ArrayList<>());
         }
+        int indegree[] = new int[n];
         for(int i=0 ; i<n ; i++)
         {
-            for(int a : graph[i])
+            for(int e : graph[i])
             {
-                adj.get(a).add(i);
+                adj.get(e).add(i);
                 indegree[i]++;
             }
         }
-
+        ArrayList<Integer> al = new ArrayList<>();
         Queue<Integer> q = new ArrayDeque<>();
         for(int i=0 ; i<n ; i++)
         {
-            if(indegree[i]==0)
-            {
-                q.offer(i);
-            }
+            if(indegree[i] == 0)
+            q.offer(i);
         }
-
-        ArrayList<Integer> al = new ArrayList<>();
         while(!q.isEmpty())
         {
             int u = q.poll();
@@ -33,17 +29,10 @@ class Solution {
             for(int v : adj.get(u))
             {
                 indegree[v]--;
-                if(indegree[v]==0)
-                {
-                    q.offer(v);
-                }
+                if(indegree[v]==0) q.offer(v);
             }
         }
         Collections.sort(al);
         return al;
     }
 }
-
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna

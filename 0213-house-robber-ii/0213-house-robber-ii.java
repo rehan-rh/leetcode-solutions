f68813078fcd9f38b604f85c5d5b454bea6ae30b
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-09-04 17:58:01 +0530

    Time: 0 ms (100%), Space: 42.7 MB (78%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,31 +1,28 @@
 class Solution {
-    int f(int nums[])
-    {
+    public int f(int[] nums) {
         int n = nums.length;
         int dp[] = new int[n];
-        if(n==0) return 0;
         dp[0] = nums[0];
-        for(int i=1 ; i<n ; i++)
+        for(int ind=1 ; ind<n ; ind++)
         {
-            int p = nums[i];
-            if(i>1) 
-            p += dp[i-2];
-            int np = 0 + dp[i-1];
-            dp[i] = Math.max(p, np);
-        } 
+            int p = nums[ind];
+            if(ind>1)
+            p = nums[ind] + dp[ind-2];
+            int np = 0 + dp[ind-1];
+            dp[ind] = Math.max(p, np);
+        }
         return dp[n-1];
     }
     public int rob(int[] nums) {
         int n = nums.length;
         if(n==1) return nums[0];
-        int temp1[] = new int[n];
-        int temp2[] = new int[n];
+        int nums1[] = new int[n];
+        int nums2[] = new int[n];
         for(int i=0 ; i<n ; i++)
         {
-            if(i!=0) temp1[i] = nums[i];
-            if(i!=n-1) temp2[i] = nums[i];
+            if(i!=0) nums1[i] = nums[i];
+            if(i!=n-1) nums2[i] = nums[i];
         }
-
-        return Math.max(f(temp1), f(temp2));
+        return Math.max(f(nums1), f(nums2));
     }
 }

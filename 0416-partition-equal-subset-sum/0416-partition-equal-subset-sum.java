66c6cbdae32e5ee288ba5631041cb905b17da8bf
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-09-04 21:31:01 +0530

    Time: 37 ms (65.38%), Space: 76 MB (14.17%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,38 +1,32 @@
 class Solution {
-    int f(int ind, int t, int dp[][], int nums[])
+    boolean f(int a[], int target)
     {
-        if(t==0) return 1;
-        if(dp[ind][t]!=-1) return dp[ind][t];
-        if(ind==0) return nums[0]==t?1:0;
-        int p = 0;
-        if(nums[ind]<=t)
-        p = f(ind-1, t-nums[ind], dp, nums);
-        int np = f(ind-1, t, dp, nums);
-        return dp[ind][t]=Math.max(p, np);
-    }
-    public boolean canPartition(int[] nums) {
-        int n = nums.length;
-        int sum = 0;
-        for(int e:nums) sum+=e;
-        if(sum%2==1) return false;
-        int dp[][] = new int[n][sum/2+1];
+        int n = a.length;
+        int dp[][] = new int[n][target+1];
         for(int i=0 ; i<n ; i++)
         {
             dp[i][0] = 1;
         }
-        if(nums[0]==sum/2)
-        dp[0][nums[0]] = 1;
+        if(a[0]<=target)
+        dp[0][a[0]] = 1;
         for(int i=1 ; i<n ; i++)
         {
-            for(int t=0 ; t<sum/2+1 ; t++)
+            for(int t=1 ; t<=target ; t++)
             {
                 int p = 0;
-                if(nums[i]<=t)
-                p = dp[i-1][t-nums[i]];
+                if(a[i]<=t)
+                p = dp[i-1][t-a[i]];
                 int np = dp[i-1][t];
                 dp[i][t] = Math.max(p, np);
             }
         }
-        return dp[n-1][sum/2]==1;
+        return dp[n-1][target]==1;
+    }
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for(int e:nums) sum += e;
+        if(sum%2==1) return false;
+        int target = sum/2;
+        return f(nums, target);
     }
 }

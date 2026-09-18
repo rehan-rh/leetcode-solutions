3588e1dbcbc4e09f8594c127e8382c071a832cb0
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-09-18 20:59:12 +0530

    Time: 6 ms (72.55%), Space: 47.3 MB (33.34%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,33 +1,30 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adj = new ArrayList<>();
         int n = numCourses;
-        for(int i=0 ; i<numCourses ; i++)
-        {
-            adj.add(new ArrayList<>());
-        }
-        int indegree[] = new int[numCourses];
+        int inDegree[] = new int[n];
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i= 0 ; i<n ; i++) adj.add(new ArrayList<>());
         Queue<Integer> q = new ArrayDeque<>();
-        for(int arr[]:prerequisites)
+        for(int arr[] : prerequisites)
         {
             int a = arr[0];
             int b = arr[1];
             adj.get(b).add(a);
-            indegree[a]++;
+            inDegree[a]++;
         }
-        for(int i=0 ; i<n ; i++)
+        for(int i=0 ; i<n ; i++) 
         {
-            if(indegree[i]==0) q.offer(i);
+            if(inDegree[i]==0) q.offer(i);
         }
         int c = 0;
         while(!q.isEmpty())
         {
             int u = q.poll();
             c++;
-            for(int v:adj.get(u))
+            for(int v : adj.get(u))
             {
-                indegree[v]--;
-                if(indegree[v]==0) 
+                inDegree[v]--;
+                if(inDegree[v]==0)
                 q.offer(v);
             }
         }

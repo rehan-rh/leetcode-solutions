53a4088e958aa46e2ad689e31dc477578e8e71ef
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-09-18 21:26:50 +0530

    Time: 14 ms (71.23%), Space: 49.8 MB (8.42%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,4 +1,4 @@
-class pair 
+class pair
 {
     int r;
     int c;
@@ -12,17 +12,17 @@ class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
-        int dis[][] = new int[n][m];
-        int vis[][] = new int[n][m];
         Queue<pair> q = new ArrayDeque<>();
+        int vis[][] = new int[n][m];
+        int dis[][] = new int[n][m];
         for(int i=0 ; i<n ; i++)
         {
             for(int j=0 ; j<m ; j++)
             {
                 if(mat[i][j]==0)
                 {
-                    vis[i][j] = 1;
                     dis[i][j] = 0;
+                    vis[i][j] = 1;
                     q.offer(new pair(i, j));
                 }
             }
@@ -34,15 +34,15 @@ class Solution {
             pair p = q.poll();
             int r = p.r;
             int c = p.c;
-            int d = dis[r][c];
-            for(int i=0 ; i<4 ; i++)
+            for(int i=0 ; i<=3 ; i++)
             {
-                int nr = row[i]+r;
-                int nc = col[i]+c;
-                if(nr>=0 && nr<n && nc>=0 && nc<m && vis[nr][nc]==0)
+                int nr = r + row[i];
+                int nc = c + col[i];
+                int d = dis[r][c];
+                if(nr>=0 && nr<n && nc>=0 && nc<m && vis[nr][nc]!=1)
                 {
-                    vis[nr][nc] = 1;
                     dis[nr][nc] = d+1;
+                    vis[nr][nc] = 1;
                     q.offer(new pair(nr, nc));
                 }
             }

93977608b30b3304b00649e1328333300e8717c8
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-09-19 19:56:30 +0530

    Time: 37 ms (65%), Space: 76 MB (13.28%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,32 +1,31 @@
 class Solution {
-    boolean f(int a[], int target)
+    boolean f(int[] arr, int target) 
     {
-        int n = a.length;
+        int n = arr.length;
         int dp[][] = new int[n][target+1];
         for(int i=0 ; i<n ; i++)
         {
             dp[i][0] = 1;
         }
-        if(a[0]<=target)
-        dp[0][a[0]] = 1;
-        for(int i=1 ; i<n ; i++)
+        if(arr[0]<=target)
+        dp[0][arr[0]] = 1;
+        for(int ind=1 ; ind<n ; ind++)
         {
-            for(int t=1 ; t<=target ; t++)
+            for(int t=1 ; t<target+1 ; t++)
             {
                 int p = 0;
-                if(a[i]<=t)
-                p = dp[i-1][t-a[i]];
-                int np = dp[i-1][t];
-                dp[i][t] = Math.max(p, np);
+                if(arr[ind]<=t)
+                p = dp[ind-1][t-arr[ind]];
+                int np = dp[ind-1][t];
+                dp[ind][t] = Math.max(p, np);
             }
         }
         return dp[n-1][target]==1;
     }
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for(int e:nums) sum += e;
+        for(int e:nums) sum+=e;
         if(sum%2==1) return false;
-        int target = sum/2;
-        return f(nums, target);
+        return f(nums, sum/2);
     }
 }

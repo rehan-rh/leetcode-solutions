af81befdcf31ef3e3dfe06d965fd761a9606c341
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-09-19 15:40:32 +0530

    Time: 0 ms (100%), Space: 42 MB (72.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,20 +1,13 @@
 class Solution {
-    int f(int ind, int []dp)
+    int f(int ind, int[] dp)
     {
         if(ind<=2) return ind;
         if(dp[ind]!=-1) return dp[ind];
         return dp[ind] = f(ind-1, dp)+f(ind-2, dp);
     }
     public int climbStairs(int n) {
-        if(n<=2) return n;
         int dp[] = new int[n+1];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
-        for(int i=3 ; i<=n ; i++)
-        {
-            dp[i] = dp[i-1]+dp[i-2];
-        }
-        return dp[n];
+        Arrays.fill(dp, -1);
+        return f(n, dp);
     }
 }

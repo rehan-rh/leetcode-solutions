9554619509eaea98602f175cf013a58bd1c17a98
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-09-21 18:48:26 +0530

    Time: 19 ms (63.64%), Space: 63.2 MB (20.46%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -0,0 +1,34 @@
+class Solution {
+    int f(int i, int j, String s, String p, int dp[][])
+    {
+        if(i<0 && j<0) return 1;
+        if(j<0 && i>=0) return 0;
+        if(i<0 && j>=0){
+            for(int k=0 ; k<=j ; k++)
+            {
+                if(p.charAt(k)!='*') return 0;
+            }
+            return 1;
+        } 
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='?')
+        {
+            return dp[i][j] = f(i-1, j-1, s, p, dp);
+        }
+        if(p.charAt(j)=='*')
+        {
+            return dp[i][j] = Math.max(f(i, j-1, s, p, dp), f(i-1 , j, s, p, dp));
+        }
+        return dp[i][j] = 0;
+
+    }
+    public boolean isMatch(String s, String p) {
+        int n = s.length();
+        int m = p.length();
+        int dp[][] = new int[n][m];
+        for(int a[]:dp) Arrays.fill(a, -1);
+        return f(n-1, m-1, s, p, dp)==1;
+    }
+}

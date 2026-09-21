32c5011c802813ce469d798d634121bc418fb9db
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-09-21 12:20:24 +0530

    Time: 18 ms (27.54%), Space: 57.4 MB (17.17%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,43 +1,23 @@
 class Solution {
-    int f(int ind, int t, int dp[][], int a[])
+    int f(int ind, int amt, int dp[][], int a[])
     {
         if(ind==0)
         {
-            if(t==0 && a[0]==0) return 2;
-            if(t==0 || t%a[ind]==0) return 1;
+            if(a[0]==0) return 2;
+            if(amt==0 || amt%a[0]==0) return 1;
             return 0;
         }
-        if(dp[ind][t]!=-1) return dp[ind][t];
+        if(dp[ind][amt]!=-1) return dp[ind][amt];
         int p = 0;
-        if(a[ind]<=t)
-        p = f(ind, t-a[ind], dp, a);
-        int np = f(ind-1, t, dp, a);
-        return dp[ind][t] = p+np;
+        if(a[ind]<=amt)
+        p = f(ind, amt-a[ind], dp, a);
+        int np = f(ind-1, amt, dp, a);
+        return dp[ind][amt] = p+np;
     }
     public int change(int amount, int[] coins) {
         int n = coins.length;
         int dp[][] = new int[n][amount+1];
-        if(coins[0]==0)
-        dp[0][0] = 2;
-        else
-        {
-            dp[0][0] = 1;
-            for(int i=0 ; i<amount+1 ; i++)
-            {
-                if(i%coins[0]==0) dp[0][i] = 1;
-            }
-        }
-        for(int i=1 ; i<n ; i++)
-        {
-            for(int t=0 ; t<amount+1 ; t++)
-            {
-                int p = 0;
-                if(coins[i]<=t)
-                p = dp[i][t-coins[i]];
-                int np = dp[i-1][t];
-                dp[i][t] = p+np;
-            }
-        }
-        return dp[n-1][amount];
+        for(int a[]:dp) Arrays.fill(a, -1);
+        return f(n-1, amount, dp, coins);
     }
 }

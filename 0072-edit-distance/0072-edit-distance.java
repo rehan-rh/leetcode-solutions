42ec222a7ec965eb9ddd967e18a82629f6342efa
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-09-21 17:55:29 +0530

    Time: 5 ms (66.11%), Space: 47.4 MB (6.18%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -14,7 +14,20 @@ class Solution {
         int n = word1.length();
         int m = word2.length();
         int dp[][] = new int[n+1][m+1];
-        for(int a[]:dp) Arrays.fill(a, -1);
-        return f(n, m, word1, word2, dp);
+        for(int j=0 ; j<=m ; j++) dp[0][j] = j;
+        for(int i=1 ; i<=n ; i++) dp[i][0] = i;
+        for(int i=1 ; i<=n ; i++)
+        {
+            for(int j=1 ; j<=m ; j++)
+            {
+                if(word1.charAt(i-1)==word2.charAt(j-1))
+                    dp[i][j] = dp[i-1][j-1];
+                else
+                {
+                    dp[i][j] = 1 + Math.min(dp[i][j-1], Math.min(dp[i-1][j], dp[i-1][j-1]));
+                }
+            }
+        }
+        return dp[n][m];
     }
 }

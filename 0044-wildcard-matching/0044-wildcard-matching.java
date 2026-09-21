800c2c3260fbd052155cb683e85085441adac40b
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-09-21 19:13:44 +0530

    Time: 53 ms (24.8%), Space: 62.7 MB (25.49%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -2,8 +2,8 @@ class Solution {
     int f(int i, int j, String s, String p, int dp[][])
     {
         if(i==0 && j==0) return 1;
-        if(j==0 && i>=0) return 0;
-        if(i==0 && j>=0){
+        if(j==0 && i>0) return 0;
+        if(i==0 && j>0){
             for(int k=1 ; k<=j ; k++)
             {
                 if(p.charAt(k-1)!='*') return 0;
@@ -28,7 +28,36 @@ class Solution {
         int n = s.length();
         int m = p.length();
         int dp[][] = new int[n+1][m+1];
-        for(int a[]:dp) Arrays.fill(a, -1);
-        return f(n, m, s, p, dp)==1;
+        dp[0][0] = 1;
+        for(int i=1 ; i<=n ; i++) dp[i][0] = 0;
+        for(int j=1 ; j<=m ; j++)
+        {
+            dp[0][j] = 1;
+            for(int k=1 ; k<=j ; k++)
+            {
+                if(p.charAt(k-1)!='*'){
+                    dp[0][j] = 0;
+                    break;
+                }
+            }
+              
+        }
+
+        for(int i=1 ; i<=n ; i++)
+        {
+            for(int j=1 ; j<=m ; j++)
+            {
+                if(s.charAt(i-1)==p.charAt(j-1) || p.charAt(j-1)=='?')
+                {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(p.charAt(j-1)=='*')
+                {
+                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+                }
+                else dp[i][j] = 0;
+            }
+        }
+        return dp[n][m]==1;
     }
 }

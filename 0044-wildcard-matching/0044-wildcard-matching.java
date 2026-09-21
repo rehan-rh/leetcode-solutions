179d4011ed525366204a952bc1400147378de959
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-09-21 18:50:28 +0530

    Time: 20 ms (61.36%), Space: 62.7 MB (25.49%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,23 +1,23 @@
 class Solution {
     int f(int i, int j, String s, String p, int dp[][])
     {
-        if(i<0 && j<0) return 1;
-        if(j<0 && i>=0) return 0;
-        if(i<0 && j>=0){
-            for(int k=0 ; k<=j ; k++)
+        if(i==0 && j==0) return 1;
+        if(j==0 && i>=0) return 0;
+        if(i==0 && j>=0){
+            for(int k=1 ; k<=j ; k++)
             {
-                if(p.charAt(k)!='*') return 0;
+                if(p.charAt(k-1)!='*') return 0;
             }
             return 1;
         } 
 
         if(dp[i][j]!=-1) return dp[i][j];
 
-        if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='?')
+        if(s.charAt(i-1)==p.charAt(j-1) || p.charAt(j-1)=='?')
         {
             return dp[i][j] = f(i-1, j-1, s, p, dp);
         }
-        if(p.charAt(j)=='*')
+        if(p.charAt(j-1)=='*')
         {
             return dp[i][j] = Math.max(f(i, j-1, s, p, dp), f(i-1 , j, s, p, dp));
         }
@@ -27,8 +27,8 @@ class Solution {
     public boolean isMatch(String s, String p) {
         int n = s.length();
         int m = p.length();
-        int dp[][] = new int[n][m];
+        int dp[][] = new int[n+1][m+1];
         for(int a[]:dp) Arrays.fill(a, -1);
-        return f(n-1, m-1, s, p, dp)==1;
+        return f(n, m, s, p, dp)==1;
     }
 }

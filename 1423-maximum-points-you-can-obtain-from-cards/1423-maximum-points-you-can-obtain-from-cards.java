2491b6f3ddd645e66f43e47f163621e0371ce6f3
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-09-23 14:36:03 +0530

    Time: 2 ms (99.69%), Space: 61.6 MB (86.34%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -10,9 +10,9 @@ class Solution {
         }
         ms = ls;
         int ri = n-1;
-        for(int i=0 ; i<k ; i++)
+        for(int i=k-1 ; i>=0 ; i--)
         {
-            ls -= cardPoints[k-1-i];
+            ls -= cardPoints[i];
             rs += cardPoints[ri];
             ri--;
             ms = Math.max(ms, ls+rs);
@@ -20,7 +20,3 @@ class Solution {
         return ms;
     }
 }
-
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

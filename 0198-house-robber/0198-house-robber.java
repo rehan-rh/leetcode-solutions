6a13100e2eb607390bb06c8333281400b85c992e
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-09-23 20:57:02 +0530

    Time: 0 ms (100%), Space: 42.8 MB (56.56%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,14 +1,4 @@
 class Solution {
-    int f(int ind, int dp[], int a[])
-    {
-        if(ind==0) return a[0];
-        if(dp[ind]!=-1) return dp[ind];
-        int p = a[ind];
-        if(ind>1)
-        p = a[ind] + f(ind-2, dp, a);
-        int np = 0 + f(ind-1, dp, a);
-        return dp[ind] = Math.max(p, np);
-    }
     public int rob(int[] nums) {
         int n = nums.length;
         int dp[] = new int[n];

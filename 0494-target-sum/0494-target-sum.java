39230cb95d307c50f9d7d7bffb55c104b615ab0f
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-09-23 23:08:43 +0530

    Time: 5 ms (80.55%), Space: 45.1 MB (45.01%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,49 +1,36 @@
 class Solution {
-    int f(int ind, int t, int dp[][], int a[])
+    int f(int a[], int target)
     {
-        if(ind==0){
-            if(t==0 && a[0]==0) return 2;
-            if(t==0 || a[0]==t) return 1;
-            return 0;
+        int n = a.length;
+        int dp[][] = new int[n][target+1];
+        if(a[0]==0)
+        {
+            dp[0][0] = 2;
         }
-        if(dp[ind][t]!=-1) return dp[ind][t];
-        int p = 0;
-        if(a[ind]<=t)
-        p = f(ind-1, t-a[ind], dp, a);
-        int np = f(ind-1, t, dp, a);
-        return dp[ind][t] = p+np;
-    }
-    int df(int nums[], int dif)
-    {
-        int ts = 0;
-        for(int e:nums) ts += e;
-        int t = (ts-dif)/2;
-        if((ts-dif)<0 || (ts-dif)%2==1) return 0;
-        int n = nums.length;
-        int dp[][] = new int[n][t+1];
-        if(nums[0]==0)
-        dp[0][0] = 2;
         else
         {
             dp[0][0] = 1;
-            if(nums[0]<=t)
-            dp[0][nums[0]] = 1;
+            if(a[0]<=target)
+            dp[0][a[0]] = 1;
         }
-        for(int i=1 ; i<n ; i++)
+
+        for(int ind=1 ; ind<n ; ind++)
         {
-            for(int j=0 ; j<t+1 ; j++)
+            for(int t=0 ; t<=target ; t++)
             {
                 int p = 0;
-                if(nums[i]<=j)
-                p = dp[i-1][j-nums[i]];
-                int np = dp[i-1][j];
-                dp[i][j] = p+np;
+                if(a[ind]<=t)
+                p = dp[ind-1][t-a[ind]];
+                int np = dp[ind-1][t];
+                dp[ind][t] = p+np;
             }
         }
-        return dp[n-1][t];
-
+        return dp[n-1][target];
     }
     public int findTargetSumWays(int[] nums, int target) {
-        return df(nums, target);
+        int sum = 0;
+        for(int e:nums) sum += e;
+        if((sum-target)%2==1 || sum-target<0) return 0;
+        return f(nums, (sum-target)/2);
     }
 }

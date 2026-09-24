e85a4841150cdd7900b118e932b7155a3f42b8b6
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-09-24 18:39:47 +0530

    Time: 30 ms (20.96%), Space: 47.1 MB (26.08%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,39 +1,23 @@
 class Solution {
     int f(int ind, int amt, int dp[][], int a[])
     {
-        if(ind==0)
-        {
+        if(ind==0){
             if(amt%a[0]==0) return amt/a[0];
             return (int)1e9;
         }
         if(dp[ind][amt]!=-1) return dp[ind][amt];
         int p = (int)1e9;
         if(a[ind]<=amt)
-        p = 1+f(ind, amt-a[ind], dp, a);
+        p = 1 + f(ind, amt-a[ind], dp, a);
         int np = f(ind-1, amt, dp, a);
         return dp[ind][amt] = Math.min(p, np);
+
     }
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
         int dp[][] = new int[n][amount+1];
-        for(int i=0 ; i<amount+1 ; i++)
-        {
-            if(i%coins[0]==0)
-            dp[0][i] = i/coins[0];
-            else
-            dp[0][i] = (int)1e9;
-        }
-        for(int ind=1 ; ind<n ; ind++)
-        {
-            for(int a=0 ; a<amount+1 ; a++)
-            {
-                int p = (int)1e9;
-                if(coins[ind]<=a)
-                p = 1 + dp[ind][a-coins[ind]];
-                int np = dp[ind-1][a];
-                dp[ind][a] = Math.min(p, np);
-            }
-        }
-        return dp[n-1][amount]==(int)1e9?-1:dp[n-1][amount];
+        for(int a[]:dp) Arrays.fill(a, -1);
+        int fu = f(n-1, amount, dp, coins);
+        return fu==(int)1e9?-1:fu;
     }
 }

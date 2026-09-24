793fd27011eeb43a108416318897418cde39769a
Pinjari Rehan Hussain <rehanhussain0842@gmail.com>
2026-09-24 20:47:55 +0530

    Time: 1221 ms (80.41%), Space: 0B (100%) - LeetHub

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -1,8 +1,4 @@
 # Write your MySQL query statement below
-select u.unique_id, e.name
-from Employees e left join EmployeeUNI u
-on e.id = u.id
-
--- Synced seamlessly with LeetHub Pro
--- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
--- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
+select eu.unique_id as unique_id, e.name as name
+from Employees e left join EmployeeUNI eu
+on e.id = eu.id 
